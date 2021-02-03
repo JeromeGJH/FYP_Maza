1dcf5f9be8e40f9828c89a8cfbc2272f4561b339
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-02-03 22:51:47 +0800

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,1 +1,44 @@
+import math
+import  numpy as np
+
+
+
+
+
+
+class Maze:
+    shape = (3, 8)
+    def __init__(self, shape, num):
+        maze = np.array(shape)
+        num_agent = num
+        num_goal = num
+
+    def initialize(self):
+
+    def setAgents(self):
+
+    def setGoals(self):
+
+
+    def train(self):
+
+    def getPayoff(self, agent):
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    maze = Maze()
+    maze
 
