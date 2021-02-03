9a2a378c10b1756e4acc4d916db5c5829054d4b1
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-02-03 22:52:31 +0800

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class Agent():
+    # 1 = left; 2 = up; 3 = right; 4 = down; 0 = stay
+    actions = (0, 1, 2, 3, 4)
+
+    def __init__(self, name, p):
+        self.position = np.array(p, dtype=int)
+        self.distance = 0
+        self.direction = []
+        self.name = name
+        self.reward = 0
+        self.ifOnCircle = False
+        self.strategy = []
+        self.actions = np.array([[0, 0], [-1, 0], [0, 1], [1, 0], [0, -1]], dtype=int)
+
+    def move(self, action):
+        self.position = self.position + self.actions[action]
+
+        # if action == Agent.actions[0]:
+        #     self.position[1] += 1
+        # elif action == Agent.actions[1]:
+        #     self.position[1] -= 1
+        # elif action == Agent.actions[2]:
+        #     self.position[0] -= 1
+        # elif action == Agent.actions[3]:
+        #     self.position[0] += 1
